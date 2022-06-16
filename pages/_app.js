e788010750316bc13fe97341ca514240cc131fb2
Bogdan-Marius Mosica <bogdan.mosica@gmail.com>
2022-06-16 18:08:07 +0300

    pdf-search beta files

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/globals.css'
+import '@react-pdf-viewer/core/lib/styles/index.css';
+import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
